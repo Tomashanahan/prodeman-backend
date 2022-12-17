c69ba913fdd9668aa500efcbb4e8c7b6a172d83f
Tomas Shanahan <tshmg@hotmail.com>
2022-12-17 20:18:41 -0300

    chore: :card_file_box: addding the connection to the database with TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
