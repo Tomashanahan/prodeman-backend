37dcde82b3acaadc64c9fcab27a3deefa9cb6cb9
Tomas Shanahan <tshmg@hotmail.com>
2022-12-17 20:21:27 -0300

    chore: :memo: adding Swagger as a documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Prodeman RESFTful API')
+    .setDescription('The Prodeman API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
